5534baf87db872205789ebe5a3b022e41f196bee
Ahmad wannous <ahmadwanous642@gmail.com>
2025-01-06 02:32:50 +0200

    GroceryItems&Pop

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'package:shopping_list/data/categories.dart';
 import 'package:shopping_list/models/category.dart';
+import 'package:shopping_list/models/grocery_item.dart';
 
 class NewItem extends StatefulWidget {
   const NewItem({super.key});
@@ -21,6 +22,14 @@ class _NewItemState extends State<NewItem> {
   void _saveItem() {
     if (_keyForm.currentState!.validate()) {
       _keyForm.currentState!.save();
+      Navigator.of(context).pop(
+        GroceryItem(
+          id: DateTime.now().toString(),
+          name: _entredName,
+          quantity: _entredQuantity,
+          category: _selectedCategory,
+        ),
+      );
     }
   }
 
