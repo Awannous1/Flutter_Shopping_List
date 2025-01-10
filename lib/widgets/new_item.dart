11e66cc8a7a6951eb5f91b698cf50aca7b3f3d7c
Ahmad wannous <ahmadwanous642@gmail.com>
2025-01-10 19:21:58 +0200

    Firebase&Http&Url

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -1,8 +1,12 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+
+import 'package:http/http.dart' as http;
 
 import 'package:shopping_list/data/categories.dart';
 import 'package:shopping_list/models/category.dart';
-import 'package:shopping_list/models/grocery_item.dart';
+// import 'package:shopping_list/models/grocery_item.dart';
 
 class NewItem extends StatefulWidget {
   const NewItem({super.key});
@@ -22,14 +26,20 @@ class _NewItemState extends State<NewItem> {
   void _saveItem() {
     if (_keyForm.currentState!.validate()) {
       _keyForm.currentState!.save();
-      Navigator.of(context).pop(
-        GroceryItem(
-          id: DateTime.now().toString(),
-          name: _entredName,
-          quantity: _entredQuantity,
-          category: _selectedCategory,
+      final url = Uri.https('flutter-pro-1-13f1c-default-rtdb.firebaseio.com',
+          'shopping-list.json');
+      http.post(
+        url,
+        headers: {'Content-Type': 'application/json'},
+        body: json.encode(
+          {
+            'name': _entredName,
+            'quantity': _entredQuantity,
+            'category': _selectedCategory.title,
+          },
         ),
       );
+      // Navigator.of(context).pop();
     }
   }
 
